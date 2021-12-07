daab36d766c03c21c4b76169fafd3e2acbe871fc
Mathias Polligkeit <git@woylie.de>
2021-12-07 18:29:24 +0900

    add sass-loader, css-loader and sass

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,26 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                mode: "icss",
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
   },
 };

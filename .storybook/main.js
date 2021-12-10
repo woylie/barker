ebe5c19af515a2d46f44b474b0cc0319bc400526
Mathias Polligkeit <git@woylie.de>
2021-12-11 00:03:56 +0900

    load css into storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,23 @@
+const path = require("path");
+
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
-  "framework": "@storybook/web-components",
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/web-components",
   core: {
-    builder: "webpack5"
-  }
+    builder: "webpack5",
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.(sa|sc|c)ss$/i,
+      use: [
+        { loader: "css-loader" },
+        { loader: "postcss-loader" },
+        { loader: "sass-loader" },
+      ],
+      include: path.resolve(__dirname, "../src/css"),
+    });
+
+    return config;
+  },
 };

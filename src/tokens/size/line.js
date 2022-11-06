62d23cd756a95708f8a694b5b703c6ccc8240fca
Mathias Polligkeit <git@woylie.de>
2022-11-06 13:40:23 +0900

    define lines with style dictionary

---
+++ src/tokens/size/line.js
@@ -0,0 +1,16 @@
+// in rem
+const baseLineHeight = 1.5;
+
+// multiples of the base line height
+const factors = [0.25, 0.5, 0.75, 1, 1.5, 2];
+
+var lines = factors.reduce(
+  (a, v) => ({ ...a, [v]: { value: baseLineHeight * v } }),
+  {}
+);
+
+module.exports = {
+  size: {
+    line: lines,
+  },
+};
